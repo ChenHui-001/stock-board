41cd07ad771901f76c77a37f08895bc3a96bb1ac
ChenHui-001 <1024928960@qq.com>
2026-08-28 16:14:50 +0800

    auto-commit: smoke_test passed | changes: M backend/providers/__init__.py

--- backend/providers/__init__.py
+++ backend/providers/__init__.py
@@ -33,6 +33,7 @@ from .base import (
     limiter,
 )
 from .eastmoney import EastmoneyProvider
+from .netease import NeteaseProvider
 from .sina import SinaProvider
 from .tencent import TencentProvider
 from .ths import ThsProvider
@@ -44,6 +45,7 @@ _FACTORIES: dict[str, Callable[[], Provider]] = {
     "tencent": TencentProvider,
     "sina": SinaProvider,
     "ths": ThsProvider,
+    "netease": NeteaseProvider,
 }
 
 if settings.ENABLE_AKSHARE:
